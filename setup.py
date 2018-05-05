951302c4e621fb3dc83c436246db7f2fe1860acd
jitesh <jitesh.mohite@kratin.co.in>
2018-05-05 18:36:35 +0530

    Code clean up

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='Gender-Predictor',  # This is the name of your PyPI-package.
-    version='0.1',  # Update the version number for new releases
+    version='0.2',  # Update the version number for new releases
     scripts=['GenderClassifier']  # The name of your script, and also the command you'll be using for calling it
 )
